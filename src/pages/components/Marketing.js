82e036038d12e5ed736d5592a79788aa9446abf2
Batkhishig <batkhishig.n@infinite.mn>
2022-06-14 21:01:36 +0800

    test

---
+++ src/pages/components/Marketing.js
@@ -0,0 +1,16 @@
+  const headerStyle = {
+    backgroundColor: "#eaeaea",
+    color: "black",
+    width: '50px',
+    writingMode: 'vertical-rl',
+    transform: 'rotate(-180deg)',
+    borderRadius: '8px',
+  };
+  
+  const Header = () => (
+    <div className="Header" style={headerStyle}>
+      MARKETING
+    </div>
+  );
+  
+  export default Header;
---
+++ src/pages/components/Navbar.js
@@ -0,0 +1,15 @@
+const navBarStyle = {
+    backgroundColor: "#eaeaea",
+    color: "black",
+    height: "50px",
+    borderRadius: '8px',
+    marginTop: '10px',
+  };
+  
+  const NavBar = () => (
+    <div className="NavBar" style={navBarStyle}>
+      NAVBAR
+    </div>
+  );
+  
+  export default NavBar;
