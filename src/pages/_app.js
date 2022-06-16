7e01c0412c8560f3f5181a3fcb806ac77fe2968c
mugi131 <munkhbayar.mugi@gmail.com>
2022-06-17 02:32:53 +0800

    merged state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,19 +4,50 @@ import Head from "next/head";
 import { ThemeProvider } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../components/theme";
-// import "../../public/style.css";
+import "../../public/style.scss";
 import "react-awesome-slider/src/styled/fold-out-animation/fold-out-animation.scss";
 import { wrapper, store } from "../../store/store";
 import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }) {
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  };
+
+  render() {
+    const { Component, pageProps } = this.props;
+
   return (
     <>
+    <Head>
+          <title>Home</title>
+        </Head>
+        <ThemeProvider theme={theme}>
       <Provider store={store}>
+      <CssBaseline />
         <Component {...pageProps} />
       </Provider>
+      </ThemeProvider>
+      <style jsx global>{`
+          :global(.MuiButton-root) {
+            letter-spacing: 0.0892857143em;
+          }
+          :global(html),
+          :globa(body),
+          :global(#__next) {
+            overflow: auto;
+            min-height: 100vh;
+            position: relative;
+            overflow-x: hidden;
+          }
+        `}</style>
     </>
   );
+}
 }
 
 export default wrapper.withRedux(MyApp);
---
+++ src/pages/components/Layout.js
@@ -0,0 +1,44 @@
+
+const layoutStyle = {
+  display: "flex",
+  flexDirection: 'row',
+  height: "430px",
+  width: "1000px",
+  left: '50%',
+  top: '50%',
+  position: 'absolute',
+  transform: 'translate(-50%, -50%)',
+  borderRadius: '8px',
+};
+
+const contentStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  height: '100%',
+  borderRadius: '8px',
+  backgroundColor: 'sienna',
+};
+
+const layoutChildren = {
+  width: '100%',
+  height: '100%',
+  paddingRight: '15px',
+  borderRadius: '8px',
+}
+
+export default function Layout(props) {
+  return (
+    <>
+        <div className="Layout-container" style={layoutStyle}>
+            <div className="Layout-children" style={layoutChildren}>
+                {/* <Header /> */}
+                <div className="Content" style={contentStyle}>
+                    {props.children}
+                </div>
+                {/* <NavBar /> */}
+            </div>
+        </div>
+    </>
+  )
+}
